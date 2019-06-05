d10fb2ada80aa6e660cafdd8fc804963bf25ba2d
Matías Dieguez <matiasdieguez@hotmail.com>
2019-06-05 12:56:58 -0300

    Agregado de proyecto DieguezArgentinaImmigration

---
+++ MDSite/MDSite/Models/DieguezImmigration.cs
@@ -0,0 +1,17 @@
+namespace MDSite.Models
+{
+    public class DieguezImmigration
+    {
+        public string Apellido { get; set; }
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string EstadoCivil { get; set; }
+        public string Nacionalidad { get; set; }
+        public string LugarDeNacimiento { get; set; }
+        public string Profesion { get; set; }
+        public string FechaDeArribo { get; set; }
+        public string Barco { get; set; }
+        public string Puerto { get; set; }
+    }
+
+}
---
+++ MDSite/MDSite/Pages/DieguezArgentinaImmigrations.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using MDSite.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+
+namespace MDSite.Pages
+{
+    public class DieguezArgentinaImmigrationsModel : PageModel
+    {
+        public List<DieguezImmigration> DieguezImmigrations { get; set; }
+        public void OnGet()
+        {
+            var path = ((IHostingEnvironment)HttpContext.RequestServices.GetService(typeof(IHostingEnvironment))).ContentRootPath;
+            var jsonFile = path + "\\wwwroot\\data\\dieguez.json";
+            var json = System.IO.File.ReadAllText(jsonFile);
+
+            var data = JsonConvert.DeserializeObject<List<DieguezImmigration>>(json);
+
+            DieguezImmigrations = data;
+        }
+    }
+}
